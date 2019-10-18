7410400e1c09c9a13a22adcd0d6824198f979471
Skyler Torian <skymanat7@yahoo.com>
2019-10-18 16:24:27 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,8 @@ def bonus
   }
 
   #code your solution here:
-  epic_tragedy = {hero: "Romeo", status: "dead"}
-  epic_tragedy = {heroine: "Juliet", status: "dead"}
+  epic_tragedy[:montague][:hero][:status] = "dead",
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
   
   epic_tragedy[:hero]
   epic_tragedy[:heroine]
